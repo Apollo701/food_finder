4471a01bec4af76522eec7f12840f240b25cf7c2
Jason Dorn <jdorn@mail.sfsu.edu>
2015-01-17 00:32:48 -0800

    fixed bugs and re-formatted output

--- lib/guide.rb
+++ lib/guide.rb
@@ -1,4 +1,5 @@
 require 'restaurant'
+require 'support/string_extend'
 class Guide
 	
 	class Config
@@ -48,7 +49,7 @@ class Guide
 		when 'list'
 			list
 		when 'find'
-			puts 'Finding...'
+			find
 		when 'add'
 			add
 		when 'quit'
@@ -59,11 +60,9 @@ class Guide
 	end
 
 	def list
-		puts "\nListing restaurants\n\n".upcase
+		output_action_header("Listing Restaurants")
 		restaurants = Restaurant.saved_restaurants
-		restaurants.each do |rest|
-			puts rest.name + " | " + rest.cuisine + " | " + rest.formatted_price
-		end
+		output_restaurant_table(restaurants)
 	end
 
 	def add
@@ -84,6 +83,27 @@ class Guide
 
 	def conclusion
 		puts "\n<< Goodbye and Bon Appetit! >>\n\n\n"
+	end
+
+	private
+
+	def output_action_header(text)
+		puts "\n#{text.upcase.center(60)}\n\n"
+	end
+
+	def output_restaurant_table(restaurants=[])
+		print " " + "Name".ljust(30)
+		print " " + "Cuisine".ljust(20)
+		print " " + "Price".rjust(6) + "\n"
+		puts "-" * 60
+		restaurants.each do |rest|
+			line = " " << rest.name.ljust(30)
+			line << " " + rest.cuisine.ljust(20)
+			line << " " + rest.formatted_price.rjust(6)
+			puts line
+		end
+		puts "No listings found" if restaurants.empty?
+		puts "-" * 60
 	end
 
 end
