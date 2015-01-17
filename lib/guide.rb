8b2644c55fea61f52f41f2b569b2334ba4fb5ec2
Jason Dorn <jdorn@mail.sfsu.edu>
2015-01-16 21:06:24 -0800

    added do_action(action) case action when list, find, add, quit

--- lib/guide.rb
+++ lib/guide.rb
@@ -19,10 +19,28 @@ class Guide
 	def launch!
 		introduction
 		# action loop
+		loop do 
 		# 	what do you want to do? (list, find, add, quit)
+		print "> "
+		user_response = gets.chomp
 		# 	do that action
+		result = do_action(user_response)
 		# repeat until user quits
+		break if result == :quit
+	end
 		conclusion
+	end
+
+	def do_action(action)
+		case action
+		when 'list'
+			puts 'Listing...'
+		when 'find'
+			puts 'Finding...'
+		when 'add'
+			puts 'Adding...'
+		when 'quit'
+			return :quit
 	end
 
 	def introduction
