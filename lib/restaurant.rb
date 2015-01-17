154e63b97fce8616378700e4d35f1a406991de04
Jason Dorn <jdorn@mail.sfsu.edu>
2015-01-16 20:57:08 -0800

    created restaurant.rb class

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -6,17 +6,27 @@ class Restaurant
 		@@filepath = File.join(APP_ROOT, path)
 	end
 
-	def self.file_exists?
-		# class should know if restaurant file exists
-		if @@filepath && File.exists?(@@filepath)
-			return true
-		else
-			return false
-		end
+	# def self.file_exists?
+	# 	# class should know if restaurant file exists
+	# 	if @@filepath && File.exists?(@@filepath)
+	# 		return true
+	# 	else
+	# 		return false
+	# 	end
+	# end
+
+	def self.file_usable?
+		return false unless @@filepath
+		return false unless File.exists?(@@filepath)
+		return false unless File.readable?(@@filepath)
+		return false unless File.writeable?(@@filepath)
+		return true
 	end
 
 	def self.create_file
 		# create the restaurant file if it doesnt exist
+		File.open(@@filepath, 'w') unless file_exists?
+		return file_usable?
 	end
 
 	def self.saved_restaurants
